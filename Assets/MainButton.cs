ce558ec1bf9983d38dcdafb22b1a1788d561b1ee
Ragekit <bgattet@gmail.com>
2018-01-23 16:22:00 -0500

    First Commit and barebone prototype Started a unity project and added a very first barebone prototype. Now to work on it a bit more.

---
+++ Assets/MainButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class MainButton : MonoBehaviour {
+
+	// Use this for initialization
+	public UnityEvent WhenOn;
+	public UnityEvent WhenOff;
+	Toggle t;
+	
+	// Update is called once per frame
+	void Start(){
+		t = GetComponent<Toggle>();
+	}
+
+	void Update () {
+		if(t.isOn){
+			WhenOn.Invoke();
+		}else {
+			WhenOff.Invoke();
+		}
+	}
+
+}
---
+++ Assets/PowerSlider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class PowerSlider : MonoBehaviour {
+	Slider s;
+	public UnityEvent OnEmpty;
+	public UnityEvent OnFull;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		s = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if(s.value <= 0){
+			OnEmpty.Invoke();
+		}else if(s.value == 1){
+			OnFull.Invoke();
+		}
+
+
+	}
+
+	public void Decrease(){
+		s.value -= speed;
+	}
+
+	
+}
